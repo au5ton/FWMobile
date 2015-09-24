f17514fa1ca2b99fc4c212389acab0494a9d043a
Austin Jackson <au5ton@icloud.com>
2015-09-23 23:46:45 -0500

    Set icons and tried to get session ID

--- FWMobile/FWMobile/LoginViewController.swift
+++ FWMobile/FWMobile/LoginViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class LoginViewController: UIViewController, UITextFieldDelegate {
 
@@ -17,8 +19,8 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         self.view.addGestureRecognizer(tapView)
         
         usernameTextField.delegate = self
+        activityIndicator.alpha = 0
         
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -35,10 +37,57 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         return true
     }
     
+    let FW_IP = "http://108.197.28.233"
+    
     @IBOutlet weak var usernameTextField: UITextField!
-
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var passwordTextField: UITextField!
     
+    @IBAction func loginButtonPressed(sender: UIButton) {
+        
+        if(usernameTextField.hasText() && passwordTextField.hasText()) {
+            self.activityIndicator.alpha = 1
+            
+            let headers = [
+                "Host": "108.197.28.233",
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:40.0) Gecko/20100101 Firefox/40.0",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                "Accept-Language": "en-US,en;q=0.5",
+                "Accept-Encoding": "gzip, deflate",
+                "DNT": "1",
+                "Connection": "keep-alive",
+                "Cookie": ""
+            ]
+            
+            
+            
+            
+            /*Alamofire.request(.GET, FW_IP+"/", headers: headers)
+                .response { request, response, data, error in
+                    
+                    
+                    if let theError = error {
+                        print(theError)
+                        
+                    }
+                    
+                    if let httpRequest = request {
+                        print(httpRequest)
+                        
+                    }
+                    
+                    if let httpResponse = response {
+                        print(httpResponse.allHeaderFields)
+                    }
+                }
+            */
+    
+            }
+        }
+        
+    }
+
+
     
     
     /*
@@ -50,5 +99,3 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         // Pass the selected object to the new view controller.
     }
     */
-
-}
