2d49d9e59b98fb87260dfaa139ad45a41d08cf08
Austin Jackson <au5ton@icloud.com>
2015-09-24 20:59:30 -0500

    Added more graphical stuff

--- FWMobile/FWMobile/FirstViewController.swift
+++ FWMobile/FWMobile/FirstViewController.swift
@@ -11,13 +11,25 @@ import UIKit
 class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        let background = UIImage(named: "Background")
+        let backgroundView = UIImageView(image: background)
+        self.view.addSubview(backgroundView)
+        self.view.sendSubviewToBack(backgroundView)
+        
+        super.viewDidLoad()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
     }
 
     @IBAction func buttonGetStartedPressed(sender: UIButton) {
